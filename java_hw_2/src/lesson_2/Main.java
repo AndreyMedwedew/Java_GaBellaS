a2989f186b8611226e453066ba00acd2e56b558b
Andrey <andreymedwedew3@gmail.com>
2023-04-16 10:55:45 +0300

    hw 2 java

---
+++ java_hw_2/src/lesson_2/Main.java
@@ -0,0 +1,13 @@
+package lesson_2;
+
+import lesson_2.task_1.Task_1;
+import lesson_2.task_2.Task_2;
+import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
